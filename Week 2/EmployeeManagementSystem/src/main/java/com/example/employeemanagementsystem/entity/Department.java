78213dfec265b83087c476dbe3461be87d155905
Vishalch118 <vishal.ch22254@gmail.com>
2026-07-01 00:50:31 +0530

    Spring Data JPA with Hibernate Exercise 2 completed

---
+++ Week 2/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/entity/Department.java
@@ -0,0 +1,21 @@
+package com.example.employeemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+}
