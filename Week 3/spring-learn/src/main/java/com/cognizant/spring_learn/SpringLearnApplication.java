7825b56e4bf4c923b3b25ed4e2084a0c379baf26
Vishalch118 <vishal.ch22254@gmail.com>
2026-07-02 10:09:19 +0530

    Completed Mandatory hands-on 1 of Spring REST

--- Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,12 +2,20 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.cognizant.spring_learn.service.CountryService;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringLearnApplication.class, args);
+
+		CountryService countryService = new CountryService();
+
+		System.out.println(countryService.getCountry());
 	}
+
+
 
 }
---
+++ Week 3/spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,16 @@
+package com.cognizant.spring_learn.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.spring_learn.model.Country;
+
+public class CountryService {
+
+    public Country getCountry() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        return context.getBean("country", Country.class);
+    }
+}
